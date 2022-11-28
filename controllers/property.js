a53ed79720c50e5d34e8c0aa2bb65c259a8b08f1
Christian Fenn <cfenn2020@fau.edu>
2022-11-28 16:40:05 -0500

    added basic print functionality for properties

--- controllers/property.js
+++ controllers/property.js
@@ -2,33 +2,51 @@ const Property = require("../models/property")
 const {validationResult} = require('express-validator')
 
 // Creates a New Property - WORKING
-exports.addProperty = (req, res) => {
+exports.modifyProperty = (req, res) => {
     const errors = validationResult(req)
+    var action = req.body.checkAction;
+    console.log(action)
   
     if(!errors.isEmpty()) {
       console.log(errors.array()[0].msg)
     }
-  
-    const property = new Property({
-      ownerId: req.app.locals.user.id,
-      dateOfPurchase: req.body.dateOfPurchase,
-      price: req.body.price,
-      address: req.body.address,
-      zipCode: req.body.zipCode
-    })
-    property.save((err, property) => {
-      if(err) {
-        console.log(err);
-        console.log("Unable to add property")
-      } else {
-        console.log("Added Property Successfully")
-        console.log(property)
-      }
-    })
+
+    switch(action) {
+      case 'add':
+        const property = new Property({
+          ownerId: req.app.locals.user.id,
+          dateOfPurchase: req.body.dateOfPurchase,
+          price: req.body.price,
+          address: req.body.address,
+          zipCode: req.body.zipCode
+        })
+        property.save((err, property) => {
+          if(err) {
+            console.log(err);
+            console.log("Unable to add property")
+          } else {
+            console.log("Added Property Successfully")
+            console.log(property)
+          }
+        })
+        break;
+      case 'delete':
+          Property.findOneAndDelete({ ownerId: req.app.locals.user.id, address: req.body.address }, (err, property) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("Deleted property successfully");
+            }
+          })
+          break;
+        default:
+          console.log("Error modifying property")
+          break;
+    }
   }
 
   // Edit a Property - NOT WORKING
-exports.editProperty = (req, res) => {
+/*exports.editProperty = (req, res) => {
     const errors = validationResult(req)
 
     if(!errors.isEmpty()) {
@@ -70,5 +88,5 @@ exports.deleteProperty = (req, res) => {
       console.log("Deleted property successfully");
     }
   })
-}
+}*/
   
